5aceda8a3ce1593ae550fc8075ec739988c6e27a
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-04-01 16:00:05 +0100

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Umair Salam on 4/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Umair Salam on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
